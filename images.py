237c7d20133d4d0f5478ca912e2b0be97f3cd9f7
VB2004 <86616810+VB2004@users.noreply.github.com>
2022-08-02 21:52:25 +0530

    Add files via upload

---
+++ images.py
@@ -0,0 +1,32 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("Images/region.png")
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+titles = ['Image', 'RGB', 'HSV', 'LAB', 'Gray']
+images = [img, rgb, hsv, lab, gray]
+
+img.shape
+print("Height :", img.shape[0])
+print("Width :", img.shape[1])
+print("Size :", img.size)
+
+fig = plt.figure(figsize=(6,6))
+for i in range(5):
+    fig.add_subplot(2,3,i+1)
+    plt.title(titles[i])
+    plt.imshow(images[i])
+plt.show()
+
+
+#cv2.imshow("Image",img)
+#cv2.imshow("RGB",rgb)
+#cv2.imshow("HSV",hsv)
+#cv2.imshow("Lab",lab)
+#cv2.imshow("Gray",gray)
+
+cv2.waitKey(0)   
